6f8cc0d153727efa3689b638022536f5782e6250
azvenkat7 <150216425+azvenkat7@users.noreply.github.com>
2024-05-07 10:01:39 +0530

    Update variables.tf

--- terraform/modules/aks/variables.tf
+++ terraform/modules/aks/variables.tf
@@ -83,7 +83,7 @@ variable "default_node_pool_vm_size" {
 
 variable "default_node_pool_availability_zones" {
   description = "Specifies the availability zones of the default node pool"
-  default     = ["1", "2", "3"]
+  default     = ["2", "3"]
   type        = list(string)
 }
 
