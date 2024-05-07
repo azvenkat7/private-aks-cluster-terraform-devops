edc108c37272c46ed4d01d2714703d115f760816
azvenkat7 <150216425+azvenkat7@users.noreply.github.com>
2024-05-07 08:12:12 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,7 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.50"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 }
 
-terraform {
-  backend "azurerm" {
-  }
-}
 
 locals {
   storage_account_prefix = "boot"
